39f23d386caea4e1ffdf5b5d0c1ee252f30f035a
Bianka Martinovic <github.20.cerberus@spamgourmet.com>
2013-06-18 16:06:38 +0200

    initial commit

---
+++ lib_securimage/info.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or (at
+ *   your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *   General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ *   @author          Black Cat Development
+ *   @copyright       2013, Black Cat Development
+ *   @link            http://blackcat-cms.org
+ *   @license         http://www.gnu.org/licenses/gpl.html
+ *   @category        CAT_Modules
+ *   @package         lib_securimage
+ *
+ */
+
+if (defined('CAT_PATH')) {
+    if (defined('CAT_VERSION')) include(CAT_PATH.'/framework/class.secure.php');
+} elseif (file_exists($_SERVER['DOCUMENT_ROOT'].'/framework/class.secure.php')) {
+    include($_SERVER['DOCUMENT_ROOT'].'/framework/class.secure.php');
+} else {
+    $subs = explode('/', dirname($_SERVER['SCRIPT_NAME']));    $dir = $_SERVER['DOCUMENT_ROOT'];
+    $inc = false;
+    foreach ($subs as $sub) {
+        if (empty($sub)) continue; $dir .= '/'.$sub;
+        if (file_exists($dir.'/framework/class.secure.php')) {
+            include($dir.'/framework/class.secure.php'); $inc = true;    break;
+        }
+    }
+    if (!$inc) trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+}
+
+$module_directory    = 'lib_securimage';
+$module_name         = 'SecurImage PHP Captcha Library';
+$module_function     = 'library';
+$module_version      = '1.0';
+$module_platform     = '1.x';
+$module_requirements = 'PHP 5.2 or higher, GD installed and enabled';
+$module_author 		 = 'Drew Phillips (http://www.phpcaptcha.org/); Black Cat CMS Development (Module)';
+$module_home		 = 'http://blackcat-cms.org';
+$module_license 	 = 'GNU General Public License';
+$module_description  = 'SecurImage PHP Captcha Library for Black Cat CMS';
+$module_guid         = '8E18ECB6-D409-4A1A-8028-F0BCD10917E8';
+
+?>
